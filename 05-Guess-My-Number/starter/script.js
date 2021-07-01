74a4ec671211c010e777be3f3204c6fc4d79ddc4
Pufty <thepufty@gmail.com>
2021-07-01 15:21:15 +0300

    Initial changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// document.querySelector(`.message`).textContent = `Hey`;
+// document.querySelector(`.guess`).value;
+
+const number = Math.trunc(Math.random()*20);
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+    const inputValue = document.querySelector(`.guess`).value;
+    console.log(inputValue);
+    if (!inputValue) {
+        document.querySelector(`.message`).textContent = `Not a valid number`;
+    } else {
+        document.querySelector(`.message`).textContent = `You've input ${inputValue}`;
+    }
+});
